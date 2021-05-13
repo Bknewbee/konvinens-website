002d01eae273eea60f8c7a7f26fb2e29be8ed127
Bknewbee <esera93.bhuka@gmail.com>
2021-05-13 14:44:34 +0200

    test server 1

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       withCredentials: true
     }
 
-    axios.get(`https://konvinens.herokuapp.com/api/user`, config)
+    axios.get(`/api/user`, config)
       .then((res)=> {
         console.log(res.data);
         setData(res.data)
